4b0598bcda971f02d81078672a49d1f37c5ea990
ngalkov <ngalkov@users.noreply.github.com>
2018-09-07 19:18:53 +0300

    Add logging to example

--- example/application.py
+++ example/application.py
@@ -1,11 +1,14 @@
 """The main WSGI application."""
 
+import logging
+
 from vial import Vial
 
 app = Vial()
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     from wsgiref.simple_server import make_server
     with make_server('', 8000, app) as httpd:
-        print("Serving on port 8000...")
+        logging.info("Serving on port 8000...")
         httpd.serve_forever()
